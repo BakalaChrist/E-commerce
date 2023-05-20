9d4d5717cf72f782d219e139306bbb082172cdcb
Christ Bakala <119419699+BakalaChrist@users.noreply.github.com>
2023-05-20 20:02:25 +0200

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
 
                     <x-slot name="content">
                     <x-dropdown-link :href="route('acceuil')">
-                            Acceuil
+                            Home
                         </x-dropdown-link>
                         
                         <x-dropdown-link :href="route('profile.edit')">
